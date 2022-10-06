0681e8f89432358f82226308f65080da7b688fb9
yourgotocoder <yourgotocoder@gmail.com>
2022-10-06 14:32:55 +0530

    Get request handled using, very easy indeed

--- rest/main.go
+++ rest/main.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Mayer", Artist: "John Mayer", Price: 6.99},
+	{ID: "2", Title: "New Light", Artist: "John Mayer", Price: 8.99},
+	{ID: "3", Title: "Room of Squares", Artist: "John Mayer", Price: 9.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:3000")
 }
