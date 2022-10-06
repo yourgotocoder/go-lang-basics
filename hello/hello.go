952d90790ba9fdc8570e254be6a96e0d8399145b
yourgotocoder <yourgotocoder@gmail.com>
2022-10-06 14:02:11 +0530

    Multiple arguments in function

--- hello/hello.go
+++ hello/hello.go
@@ -10,9 +10,12 @@ func main() {
 	// Get a greeting message and print it.
 	log.SetPrefix("greetins: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Sudu")
+
+	names := []string{"Sudu", "Sudarshan", "Sun"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
